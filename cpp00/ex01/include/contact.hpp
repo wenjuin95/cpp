1de85e688a8bdaa25c698732de2c4f83d0d4b54b
low wen juin <lowwenjuin27@gmail.com>
2024-05-26 18:24:11 +0800

    [UPDATE]do ex01

---
+++ cpp00/ex01/include/contact.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   contract.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: welow < welow@student.42kl.edu.my>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/26 17:57:37 by welow             #+#    #+#             */
+/*   Updated: 2024/05/26 17:57:37 by welow            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+# define CONTACT_HPP
+
+#include <iostream>
+
+/*
+* class: smilar to strcut in c
+* constructor: function called when an object is created
+* destructor: function called when an object is deleted
+*/
+class Contact
+{
+	public:
+		Contact(); //constructor
+		~Contact(); //destructor
+		
+};
+
+#endif
