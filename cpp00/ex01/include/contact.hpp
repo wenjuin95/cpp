65d1f47904c779d1ff7fd487a9cb56833b708f45
low wen juin <lowwenjuin27@gmail.com>
2024-06-02 19:01:21 +0800

    [UPDATE]do contact

--- cpp00/ex01/include/contact.hpp
+++ cpp00/ex01/include/contact.hpp
@@ -26,9 +26,12 @@
 *	 especially for built-in types like int, where uninitialized variables 
 *	 can lead to undefined behavior.
 *
+* destuctor :: purpose is to perform cleanup tasks, 
+* 	 		   such as releasing resources or deallocating memory, 
+* 			   associated with the object before it is destroyed
 */
 
-//Contact :: get contact information
+//Contact :: get & print contact information
 /*
 * 1. first & last name
 * 2. nickname
@@ -44,8 +47,25 @@ class Contact
 		std::string	dark_secret;
 		int			phone_number;
 	public:
-		Contact();
-		~Contact();
+		Contact(); //default constructor
+		~Contact(); //destructor
+
+		//setter :: set the value of the contact
+		void set_first_name(std::string first_name);
+		void set_last_name(std::string last_name);
+		void set_nickname(std::string nickname);
+		void set_phone_number(int phone_number);
+		void set_dark_secret(std::string dark_secret);
+		
+		//getter :: get the value from the contact
+		std::string get_first_name();
+		std::string get_last_name();
+		std::stirng get_nickname();
+		int get_phone_number();
+		std::string get_dark_secret();
+		
+		//print contact
+		void print_contact();
 };
 
 #endif
--- cpp00/ex01/src/contact.cpp
+++ cpp00/ex01/src/contact.cpp
@@ -12,3 +12,71 @@
 
 #include "contact.hpp"
 
+//default constructor
+Contact::Contact() {}
+
+/* 
+* destuctor :: purpose is to perform cleanup tasks, 
+* 	 		   such as releasing resources or deallocating memory, 
+* 			   associated with the object before it is destroyed
+*/
+Contact::~Contact() {}
+
+Contact::set_first_name(std::string fn)
+{
+	fn = first_name;
+}
+
+Contact::set_last_name(std::string ln)
+{
+	ln = last_name;
+}
+
+Contact::set_nickname(std::string nn)
+{
+	nn = nickname;
+}
+
+Contact::set_phone_number(int pn)
+{
+	pn = phone_number;
+}
+
+Contact::set_dark_secret(std::string ds)
+{
+	ds = dark_secret;
+}
+
+Contact::get_first_name()
+{
+	return (first_name);
+}
+
+Contact::get_last_name()
+{
+	return (last_name);
+}
+
+Contact::get_nickname()
+{
+	return (nickname);
+}
+
+Contact::get_phone_number()
+{
+	return (phone_number);
+}
+
+Contact::get_dark_secret()
+{
+	return (dark_secret);
+}
+
+Contact::print_contact()
+{
+	std::cout << "First Name: " << first_name << "\n";
+	std::cout << "Last Name: " << last_name << "\n";
+	std::cout << "Nickname: " << nickname << "\n";
+	std::cout << "Phone Number: " << phone_number << "\n";
+	std::cout << "Dark Secret: " << dark_secret << "\n";
+}
