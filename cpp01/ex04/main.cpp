3fa82635ff8f4ee709485e1c8fcf13de9385a8e2
low <lowwenjuin27@gmail.com>
2024-11-16 22:17:11 +0800

    feat: cpp01 ex04 visualize

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: welow <welow@student.42kl.edu.my>          #+#  +:+       +#+        */
+/*   By: welow < welow@student.42kl.edu.my>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024-11-16 06:57:45 by welow             #+#    #+#             */
-/*   Updated: 2024-11-16 06:57:45 by welow            ###   ########.fr       */
+/*   Created: 2024/11/16 06:57:45 by welow             #+#    #+#             */
+/*   Updated: 2024/11/16 22:16:24 by welow            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 #include <string>
 #include <fstream> //for file stream
 
-static void	ft_replace_line(std::string fileName, std::string s1, std::string s2) 
+static void	ft_replace_line(std::string fileName, std::string s1, std::string s2)
 {
     std::string     ReadFileName;
     std::string     WriteFileName;
 	std::ifstream	ReadFile;
-	std::ofstream	WriteFile; 
+	std::ofstream	WriteFile;
 	std::string		line;
 	std::string		tempLine;
 	size_t			linePosition;
@@ -31,14 +31,14 @@ static void	ft_replace_line(std::string fileName, std::string s1, std::string s2
 	if (ReadFile.is_open() == false)
 		std::cout << "The input file could not be opened" << std::endl;
 	WriteFile.open(WriteFileName.c_str());
-	if (WriteFile.is_open() == false) 
+	if (WriteFile.is_open() == false)
 		std::cout << "The output file could not be opened" << std::endl;
     /**
      * "getline(file, line)" reads a line from the file
      * file : the file to read from
      * line : the string where the line will be stored
-     */ 
-	while (getline(ReadFile, line)) 
+     */
+	while (getline(ReadFile, line))
 	{
 		int	position = 0;
         /**
@@ -48,17 +48,26 @@ static void	ft_replace_line(std::string fileName, std::string s1, std::string s2
          * "std::string::npos" means the end of the string
          * this function returns the position of the first occurrence of the substring
         */
-		while ((linePosition = line.find(s1, position)) != std::string::npos) 
+	   	// std::cout << "orignal line: " << line << std::endl;//visualize
+		while ((linePosition = line.find(s1, position)) != std::string::npos)
 		{
             /**
              * "substr(position, length)" returns a new string which is a substring
              * position : the starting position of the substring
              * length : the length of the substring
-             * "linePosition + s1.size()" 
+             * "linePosition + s1.size()"
             */
 			tempLine = line.substr(linePosition + s1.size()); //remove the string that you want to replace and get the unwanted string
+			// std::cout << std::endl << "after remove with s1: " << tempLine << std::endl;//visualize
 			line.resize(linePosition); //get the string with the position the rest will be removed
+			// if (line == "") //visualize
+			// 	std::cout << "[ line: \"\"" << " ] + ";//visualize
+			// else//visualize
+			// 	std::cout << "[ line: " << line << " ] + ";//visualize
+			// std::cout << "[ s2: " << s2 << " ] + ";//visualize
+			// std::cout << "[ tempLine: " << tempLine << " ]" << std::endl;//visualize
 			line = line + s2 + tempLine;
+			// std::cout << "Modified line: " << line << std::endl << std::endl;//visualize
 			position = linePosition + s2.size();
 		}
 		WriteFile << line;
